65ebd3bbf4da743285cb3c8b6d94222841fca990
sanberg <eugenesanberg@gmail.com>
2023-06-20 11:36:19 +0300

    Initial commit

---
+++ src/main/java/io/sanberg/superheroproject/SuperheroProjectApplication.java
@@ -0,0 +1,13 @@
+package io.sanberg.superheroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroProjectApplication.class, args);
+    }
+
+}

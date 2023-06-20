87d19000ccad268b0af5362029e4e0709657d1a0
sanberg <eugenesanberg@gmail.com>
2023-06-20 18:37:37 +0300

    creation of repository

---
+++ src/main/java/io/sanberg/superheroproject/repository/SuperheroRepository.java
@@ -0,0 +1,11 @@
+package io.sanberg.superheroproject.repository;
+
+import io.sanberg.superheroproject.model.Superhero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SuperheroRepository extends JpaRepository<Superhero, Long> {
+    Optional<Superhero> findByName(String name);
+}
+
